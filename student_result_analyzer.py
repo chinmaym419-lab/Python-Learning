2e24f310f34a91bb6d07aa8fb673af87a693f314
chinmaym419-lab <chinmaym419@gmail.com>
2026-07-30 22:34:47 +0530

    Create student_result_analyzer.py

---
+++ student_result_analyzer.py
@@ -0,0 +1,13 @@
+a=str(input("enter your name : "))
+b=int(input("enter your percentage  : "))
+if b>90:
+   print("very good performance")
+elif b>80:
+   print("good performance")
+elif b>60:
+   print("improvement needed")
+else
+  print("poor performance")
+
+
+   
