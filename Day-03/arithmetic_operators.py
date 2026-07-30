89cae0d6b644cf885973eb162df2aa331457471d
chinmaym419-lab <chinmaym419@gmail.com>
2026-07-30 21:08:51 +0530

    Change variable assignment to user input

--- Day-03/arithmetic_operators.py
+++ Day-03/arithmetic_operators.py
@@ -1,6 +1,5 @@
-a = 10
-b = 3
-
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
 print("Addition:", a + b)
 print("Subtraction:", a - b)
 print("Multiplication:", a * b)
