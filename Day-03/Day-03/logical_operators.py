b5bb71d31b9df2e7f2d52122849ee9c644f03470
chinmaym419-lab <chinmaym419@gmail.com>
2026-07-30 21:34:37 +0530

    Create logical_operators.py

---
+++ Day-03/Day-03/logical_operators.py
@@ -0,0 +1,6 @@
+a=str(input("enter the name :  "))
+b=int(input("enter your age : "))
+if b>=18: 
+        print("you are eligible for voting")
+  else 
+        print("you are not eligible for voting")
