192322ffb065dc6667649efad696f31352146502
chinmaym419-lab <chinmaym419@gmail.com>
2026-07-30 21:56:04 +0530

    Create student_eligibility.py

---
+++ Day-03/student_eligibility.py
@@ -0,0 +1,13 @@
+a=str(input("enter your name")
+b=int(input("enter your age")
+if b>=18:
+      print("eligible")
+else
+      print("not eligible")
+c=float(input("enter your percentage")
+if c>=60:
+        print("eligible")
+else
+        print("not eligible")
+ overall_eligible = (b >= 18) and (c >= 60)
+print("Overall eligible:", overall_eligible)
