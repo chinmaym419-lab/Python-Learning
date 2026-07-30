140bed9ed193867e5fc447e4e9131620636e067b
chinmaym419-lab <chinmaym419@gmail.com>
2026-07-30 21:15:48 +0530

    Add comparison operators example in Python

---
+++ Day-03/Day-03/comparison_operators.py
@@ -0,0 +1,8 @@
+a=float(input("enter the first number: "))
+b=float(input("enter the second number: "))
+print("Equal:", a == b)
+print("Not Equal:", a != b)
+print("Greater:", a > b)
+print("Less:", a < b)
+print("Greater or Equal:", a >= b)
+print("Less or Equal:", a <= b)
